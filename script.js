427741e769355a2ca2b96ed4aaa870a5f0af927c
jaiswalshivani02 <139761648+jaiswalshivani02@users.noreply.github.com>
2023-09-11 19:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimalNumber) {
+  // Use toString(2) to convert to binary
+  return decimalNumber.toString(2);
 }
 
-window.decimalToBinary = decimalToBinary;
+// Test cases
+console.log(decimalToBinary(7));  // Output: "111"
+console.log(decimalToBinary(10)); // Output: "1010"
+console.log(decimalToBinary(33)); // Output: "100001"
